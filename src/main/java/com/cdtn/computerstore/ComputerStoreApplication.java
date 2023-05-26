86337bb5d694c10dfef0534c776f1e822fd17564
nguyenquan17 <quannguyen2213@gmail.com>
2023-05-27 00:00:14 +0700

    build allow cors

--- src/main/java/com/cdtn/computerstore/ComputerStoreApplication.java
+++ src/main/java/com/cdtn/computerstore/ComputerStoreApplication.java
@@ -36,7 +36,7 @@ public class ComputerStoreApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "https://quanna.shop/"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
